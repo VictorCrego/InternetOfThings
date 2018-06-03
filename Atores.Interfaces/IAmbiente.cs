84e5643eecffecc3708c72ce58303b83025b0351
Victor de Oliveira Correa Crego <victorcrego@hotmail.com>
2018-06-03 13:16:21 -0300

    03-06-2018 13:16

---
+++ Atores.Interfaces/IAmbiente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Remoting.FabricTransport;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace Atores.Interfaces
+{
+    public interface IAmbiente : IActor
+    {
+        Task MeAtivarAsync(string cliente, string ambiente, string dispositivo, int versao);
+    }
+}
